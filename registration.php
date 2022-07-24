adacfc42888dd8dbcf3648fe0da649df8e7df0df
ASHOK RAMJI SHANMUGAM <32545963+ashokramji@users.noreply.github.com>
2022-07-25 04:49:55 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Adobe_Emi
+ *
+ * @category  PHP
+ * @package   Adobe\Emi
+ * @copyright Copyright © 2022 Adobe. All rights reserved.
+ * @link      https://www.adobe.com/
+ **/
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Adobe_Emi',
+    __DIR__
+);
